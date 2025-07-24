3b4047a78894a97e65f75f154ca180c2759121ee
Mik33221 <miki103gekon@gmail.com>
2025-07-24 13:19:59 +0200

    Shelf TODO's

--- Backend/src/main/java/project/model/Shelf.java
+++ Backend/src/main/java/project/model/Shelf.java
@@ -1,11 +1,20 @@
 package project.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Shelf {
+    private static int count = 0;
     private int id;
     private int x, y;
     private List<Item> items;
+
+    public Shelf(int x, int y) {
+        this.id = count++;
+        this.items = new ArrayList<Item>();
+        this.x = x;
+        this.y = y;
+    }
 
     public boolean hasItem(String type) {
         return items.stream().anyMatch(item -> item.getType().equals(type));
@@ -22,5 +31,28 @@ public class Shelf {
         return null;
     }
 
-    // Gettery/settery
+    public int addItem(Item item) {
+        items.add(item);
+        return 1;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getID(){
+        return id;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 }
