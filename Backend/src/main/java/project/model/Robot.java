c5ed48148f227897f91a0f0e7a150d5230d9eef7
Mik33221 <miki103gekon@gmail.com>
2025-07-26 14:01:31 +0200

    Random Robot momevent

--- Backend/src/main/java/project/model/Robot.java
+++ Backend/src/main/java/project/model/Robot.java
@@ -1,21 +1,49 @@
 package project.model;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Robot implements GridEntity {
     private static int count = 0;
     private int id;
     private Cord cord;
+    private WarehouseGrid warehouseGrid;
+    private int gridWidth;
+    private int gridHeight;
     private boolean carrying;
     private Item carriedItem;
     private Cord target;
 
-    public Robot() {
+    private Cord[] moveSet = {
+            new Cord(0, 1),   // Up
+            new Cord(0, -1),  // Down
+            new Cord(-1, 0),  // Left
+            new Cord(1, 0)    // Right
+    };
+
+    public Robot(WarehouseGrid warehouseGrid) {
         this.id = count++;
+        this.gridWidth = warehouseGrid.getWidth();
+        this.gridHeight = warehouseGrid.getHeight();
         carrying = false;
         carriedItem = null;
     }
 
     public void setDestination(Cord target) {
         this.target = target;
+    }
+
+    public Cord randomNextMove(){
+        Cord randomMove = moveSet[ThreadLocalRandom.current().nextInt(moveSet.length)];
+        return randomMove;
+    }
+
+    public void permormRandomStep(){
+        Cord nextStep = randomNextMove();
+        Cord nextPosition = this.cord.add(nextStep);
+        if (nextPosition.getX() >= 0 && nextPosition.getX() < gridWidth &&
+                nextPosition.getY() >= 0 && nextPosition.getY() < gridHeight) {
+            this.cord = nextPosition;
+        }
     }
 
     public Cord findNextMove(){
