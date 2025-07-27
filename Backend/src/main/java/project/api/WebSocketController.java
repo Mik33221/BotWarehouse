4986159433de4f3a925f5e543934f0ce5418a696
Mik33221 <miki103gekon@gmail.com>
2025-07-27 19:45:57 +0200

    WebSocketController posting current app state to /topic/grid

--- Backend/src/main/java/project/api/WebSocketController.java
+++ Backend/src/main/java/project/api/WebSocketController.java
@@ -3,14 +3,20 @@ package project.api;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
+import project.model.DTO.GridDTO;
+import project.model.WarehouseGrid;
 
 @Controller
 public class WebSocketController {
+    private WarehouseGrid warehouse;
+
+    public WebSocketController(WarehouseGrid warehouse) {
+        this.warehouse = warehouse;
+    }
 
     @MessageMapping("/state")
     @SendTo("/topic/grid")
-    public String sendCurrentState() {
-        // TODO: zwróć aktualny stan całości aplikacji jako JSON
-        return "{}";
+    public GridDTO sendCurrentState() {
+        return warehouse.getGridDTO();
     }
 }
